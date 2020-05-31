2a8c0ade8f1007b57ab292aea52671c6d91bd21f
antsar <antsar@ttu.ee>
2020-05-31 22:47:39 +0300

    first

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # python_loops
+Selle ülesandega õpime tsükleid.
+Pythonis kasutatakse kaht tsüklit. For tsükkel (For loop) ja while tsükkel (while loop).
+
+Enne ülesande lahendamist soovitan lugeda mõlema tsükli kohta Pydoci.
+For tsükkel: https://ained.ttu.ee/pydoc/loop.html#for-tsukkel
+While tsükkel: https://ained.ttu.ee/pydoc/loop.html#while
+
+`def check_tea_temp(temp)`
+Teed ei saa juua kui tee on liiga kuum (üle 90 kraadi), aga tee ei tohi olla ka liiga külm (alla 50 kraadi). 
+Check_tea_temp kontrollib tee jahtumist ja annab teada kui tee on joomiseks piisavalt kuum. 
+
+Sisend temp on ühe tassi tee temperatuur.
+
+`def sort_drinkable_teas(teas, temp)`
+Funktsioon tagastab listi nendest tassidest, kus temperatuur on väiksem või võrdne antud temperatuuriga.
+Tasside sorteerimisel ei muuda funktsioon tassi temperatuuri (st. check_tea_temp funktsiooni sorteerimisel kasutama ei 
+pea). 
+
+Sisend teas on list tee temperatuuridest ja temp on nõutud tee temperatuur.
---
+++ python_loops.py
@@ -0,0 +1,44 @@
+"""
+Checks weather the tea is cool enough.
+In order to drink the tea it needs to be over 90 degrees but not less than 50 degrees.
+"""
+
+
+def check_tea_temp(temp):
+    # Can't drink tea if the temperature is still boiling but it can't be too cold.
+    if (temp < 50):
+        return "Tea is too cold!"
+    while temp > 90:
+        print("Tea needs to cool down!")
+        temp -= 1
+    return "Tea is ready to be drunk!"
+
+
+"""
+Checks which teas are ready to be drunk from the selected list of teas.
+The method will not cool down the already existing teas only checking their temperature.
+"""
+
+
+def sort_drinkable_teas(teas, temp):
+    # Sort out the list of teas where the temperature is less than or equal to the required temperature.
+    valid_teas = []
+    for tea in teas:
+        if tea <= temp:
+            valid_teas.append(tea)
+    if len(valid_teas) == 0:
+        return "No teas were cold enough"
+    return valid_teas
+
+
+if __name__ == '__main__':
+    # Check tea temp
+    print(check_tea_temp(20))  # Tea is too cold
+    print(check_tea_temp(50))  # Tea is ready to be drunk
+    print(check_tea_temp(100))  # Tea needs to cool down (This happens 10 times), Tea is ready to be drunk
+
+    # Sort teas
+    teas = [100, 20, 50, 80, 40, 99, 25, 45]
+    print(sort_drinkable_teas(teas, 50))  # 20, 50, 40, 25, 45
+    print(sort_drinkable_teas(teas, 30))  # 20, 25
+    print(sort_drinkable_teas(teas, 10))  # No teas were cold enough
